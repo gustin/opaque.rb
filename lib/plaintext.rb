1c6079e991b30244f3dec49e0084d00507dc903d
gustin <gustin@users.noreply.github.com>
2019-12-09 12:13:35 -0500

    Abstract out call to registration 1 of opaque protocol.

--- lib/plaintext.rb
+++ lib/plaintext.rb
@@ -1,23 +1,34 @@
 require 'ffi'
-require "plaintext/version"
+require 'plaintext/version'
 
-class PlaintextTotp < FFI::AutoPointer
-  def self.release(ptr)
-    Totp.free_qr(ptr)
+module Plaintext
+  class AuthN < FFI::AutoPointer
+    def self.release(ptr)
+      FII.free_qr(ptr)
+    end
+
+    def self.generate_qr(username)
+      FII.generate_qr(username)
+    end
+
+    def self.registration_1(username, password)
+      FFI.registration_1(username, password)
+    end
+
+    def to_s
+      @str ||= self.read_string.force_encoding('UTF-8')
+    end
+
+    module FII
+      class Error < StandardError; end
+
+      extend FFI::Library
+      ffi_lib '../agent/target/debug/libplaintext_agent.dylib'
+      attach_function :registration_1, [:string, :string], :void
+
+      attach_function :generate_qr, :generate_totp_qr, [:string], AuthN
+      attach_function :free_qr, :free_totp_qr, [AuthN], :void
+    end
   end
 
-  def to_s
-    @str ||= self.read_string.force_encoding('UTF-8')
-  end
-
-  module ToTp
-    class Error < StandardError; end
-
-    extend FFI::Library
-    ffi_lib '../agent/target/debug/libplaintext_agent.dylib'
-    attach_function :generate_qr, :generate_totp_qr, [:string], PlaintextTotp
-    attach_function :free_qr, :free_totp_qr, [PlaintextTotp], :void
-  end
 end
-
-
--- test/plaintext_test.rb
+++ test/plaintext_test.rb
@@ -1,12 +1,18 @@
-require "test_helper"
+require 'test_helper'
 
 class PlaintextTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Plaintext::VERSION
   end
 
+  def test_registration
+    puts "~) OPAQUE Via Ruby ~)"
+    Plaintext::AuthN::registration_1('larry', 'gawdfly')
+  end
+
+
   def test_generating_totp
     puts "~)- Ruby Land ~)-"
-    puts PlaintextTotp::ToTp.generate_qr("user 12")
+    puts Plaintext::AuthN.generate_qr("user 12")
   end
 end
