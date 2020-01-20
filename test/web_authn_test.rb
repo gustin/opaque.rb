aec6608b42b1081e5118e1480aaa7ca6ff489e04
gustin <gustin@users.noreply.github.com>
2020-01-20 17:56:41 -0500

    Add a json test vector for registering a credential.

--- test/web_authn_test.rb
+++ test/web_authn_test.rb
@@ -1,7 +1,14 @@
+require 'json'
 require 'test_helper'
 
 class WebAuthnTest < Minitest::Test
   def test_registration_challenge
-    puts Plaintext::WebAuthn.registration_challenge('jerry G')
+    puts Plaintext::WebAuthn.registration_challenge('Jerry G')
+  end
+
+  def test_register_credential
+    credential = '{"id":"Gss9igxU-3iZffReo4citavDk5c6l9YQcsFAK8OPbBTFfTah3sApRZerYS48GzcN","type":"public-key","rawId":"W29iamVjdCBBcnJheUJ1ZmZlcl0=","response":{"clientDataJSON":"W29iamVjdCBBcnJheUJ1ZmZlcl0=","attestationObject":"W29iamVjdCBBcnJheUJ1ZmZlcl0="}}'
+
+    puts Plaintext::WebAuthn.register_credential('Jerry G', credential)
   end
 end
